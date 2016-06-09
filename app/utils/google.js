1a617c1125befcb1f96f311c2d9d39242367543a
Silverhawk <sondra.silverhawk@hush.com>
2016-06-08 18:37:46 -0700

    Remove google API key from link

--- app/utils/google.js
+++ app/utils/google.js
@@ -2,7 +2,6 @@
 (function() { 
   var script = document.createElement('script');
   script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3' +
-      '&key=' + CONFIG.GOOGLE_API_KEY;
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3';
   document.body.appendChild(script);
 })();
