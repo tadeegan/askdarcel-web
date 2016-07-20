4106050c5da515fb7789980cb49dc01b067263be
Leland <leland@gplpoker.com>
2016-07-20 01:14:03 -0700

    Initial link of clicking search button to redirecting to search results

--- app/components/LargeHeader.js
+++ app/components/LargeHeader.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { images } from '../assets';
+import { browserHistory } from 'react-router'
 
 
 function LargeHeader() {
+  var submitSearch = function() {
+    browserHistory.push({
+        pathname: '/resources',
+        query: { query: document.getElementById("srch-term").value }
+    });
+  };
   return (
     <header className="header-large">
       <nav className="navbar navbar-default" role="navigation">
@@ -19,7 +26,7 @@ function LargeHeader() {
         <div className="search-container form-row" role="search">
           <input type="text" className="search-field" placeholder="I'm looking for..." name="srch-term" id="srch-term" />
           <input type="text" className="location-field" placeholder="Near me" name="srch-location" id="srch-location" />
-          <button id="largeheader_searchbutton" className="button search" type="submit"><i className="material-icons">search</i> <span>Search</span></button>
+          <button onClick={submitSearch} id="largeheader_searchbutton" className="button search" type="submit"><i className="material-icons">search</i> <span>Search</span></button>
         </div>
       </div>
     </header>
