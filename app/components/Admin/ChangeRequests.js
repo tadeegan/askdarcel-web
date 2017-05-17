70a6066c7a2143b973c8afacaf115ebce61b434f
Thomas Wolfe <tewolfe2@gmail.com>
2017-05-16 21:19:28 -0700

    Change requests edit ability (#216)
    * got payload ready
    
    * added editing to change request page
    
    * cleaned up code
    
    * added dynamic textbox sizing
    
    * tested and ready

--- app/components/Admin/ChangeRequests.js
+++ app/components/Admin/ChangeRequests.js
@@ -5,96 +5,90 @@ import ProposedService from './ProposedService';
 import Actions from './Actions';
 
 var ChangeRequests = (props) => {
-    return (
-        <div className="change-requests">
+  return (
+    <div className="change-requests">
             {renderChangeRequests(props.changeRequests, props.services, props.actionHandler)}
         </div>
-    );
+  );
 };
 
 function renderChangeRequests(changeRequests, services, actionHandler) {
-    let resourceToChangeRequests = {};
-    let resourceObjects = {};
-    let changeRequestWrappers = [];
-    changeRequests.forEach((changeRequest) => {
-        let resourceID = changeRequest.resource.id;
+  let resourceToChangeRequests = {};
+  let resourceObjects = {};
+  let changeRequestWrappers = [];
+  changeRequests.forEach((changeRequest) => {
+    let resourceID = changeRequest.resource.id;
 
-        if(!resourceToChangeRequests.hasOwnProperty(resourceID)) {
-            resourceToChangeRequests[resourceID] = [];
-        }
+    if (!resourceToChangeRequests.hasOwnProperty(resourceID)) {
+      resourceToChangeRequests[resourceID] = [];
+    }
 
-        resourceToChangeRequests[resourceID].push(changeRequest);
-        resourceObjects[resourceID] = changeRequest.resource;
-    });
+    resourceToChangeRequests[resourceID].push(changeRequest);
+    resourceObjects[resourceID] = changeRequest.resource;
+  });
 
-    let resourceToServices = {};
-    services.forEach((service) => {
-        let resourceID = service.resource.id;
-        if(!resourceToServices.hasOwnProperty(resourceID)) {
-            resourceToServices[resourceID] = [];
-        }
-        resourceToServices[resourceID].push(service);
-        resourceObjects[resourceID] = service.resource;
-    });
+  let resourceToServices = {};
+  services.forEach((service) => {
+    let resourceID = service.resource.id;
+    if (!resourceToServices.hasOwnProperty(resourceID)) {
+      resourceToServices[resourceID] = [];
+    }
+    resourceToServices[resourceID].push(service);
+    resourceObjects[resourceID] = service.resource;
+  });
 
-    for(let resourceID in resourceObjects) {
-        changeRequestWrappers.push(
-            <div key={resourceID} className="group-container">
+  for (let resourceID in resourceObjects) {
+    changeRequestWrappers.push(
+      <div key={resourceID} className="group-container">
                 <h1>{resourceObjects[resourceID].name}</h1>
                 {renderProposedServices(resourceToServices[resourceID], actionHandler)}
                 {renderIndividualRequests(resourceToChangeRequests[resourceID], actionHandler)}
             </div>
-        );
-    }
+    );
+  }
 
-    return changeRequestWrappers;
+  return changeRequestWrappers;
 }
 
 function renderProposedServices(services, actionHandler) {
-    if(!services) {
-        return;
-    }
-    return services.map((service) => {
-        return (
-            <div key={'svc'+service.id}>
-                <p>Proposed Service</p>
-                <div className="request-container">
-                    <ProposedService service={service} />
-                    <Actions
-                        id={service.id}
-                        actionHandler={actionHandler}
-                        approveAction={ChangeRequestTypes.APPROVE_SERVICE}
-                        rejectAction={ChangeRequestTypes.REJECT_SERVICE}
-                    />
-                </div>
-            </div>
-        );
-    });
+  if (!services) {
+    return;
+  }
+  return services.map((service) => {
+    return (
+      <div key={'svc'+service.id}>
+        <p>Proposed Service</p>
+        <div className="request-container">
+          <ProposedService service={service} />
+          <Actions
+            id={service.id}
+            actionHandler={actionHandler}
+            approveAction={ChangeRequestTypes.APPROVE_SERVICE}
+            rejectAction={ChangeRequestTypes.REJECT_SERVICE}
+          />
+        </div>
+      </div>
+    );
+  });
 }
 
 function renderIndividualRequests(changeRequests, actionHandler) {
-    if(!changeRequests) {
-        return;
-    }
-    let requestsToRender = [];
-    changeRequests.forEach((changeRequest) => {
-        requestsToRender.push(
-            <div key={'cr'+changeRequest.id}>
-                <p>{changeRequest.type}</p>
-                <div className="request-container">
-                    <ChangeRequest changeRequest={changeRequest} />
-                    <Actions
-                        id={changeRequest.id}
-                        actionHandler={actionHandler}
-                        approveAction={ChangeRequestTypes.APPROVE}
-                        rejectAction={ChangeRequestTypes.DELETE}
-                    />
-                </div>
-            </div>
-        );
-    });
+  if (!changeRequests) {
+    return;
+  }
+  let requestsToRender = [];
+  changeRequests.forEach((changeRequest) => {
+    requestsToRender.push(
+      <div key={'cr'+changeRequest.id}>
+        <p>{changeRequest.type}</p>
+        <div className="request-container">
+          <ChangeRequest changeRequest={changeRequest} actionHandler={actionHandler} />
+        </div>
+      </div>
+    );
+  });
 
-    return requestsToRender;
+  return requestsToRender;
 }
 
 export default ChangeRequests;
--- app/utils/DataService.js
+++ app/utils/DataService.js
@@ -1,48 +1,54 @@
 import * as _ from 'lodash/fp/object';
 
-export function post(url, body, headers) {
-    let queryHeaders = {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    };
-    if(headers) {
-        queryHeaders = _.assignIn(queryHeaders, headers);
-    }
-    return fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        headers: queryHeaders,
-        body: JSON.stringify(body)
-    }).then((resp) => {
-        setAuthHeaders(resp);
-        return resp;
-    });
+export function post(url, headers, changeRequestFields) {
+  let payload = {};
+  let body = {};
+  if (changeRequestFields) {
+    body['change_request'] = changeRequestFields;
+  }
+
+  let queryHeaders = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+  };
+  if (headers) {
+    queryHeaders = _.assignIn(queryHeaders, headers);
+  }
+  return fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    headers: queryHeaders,
+    body: JSON.stringify(body)
+  }).then((resp) => {
+    setAuthHeaders(resp);
+    return resp;
+  });
 }
 
 export function get(url, headers) {
-    let queryHeaders = {
-        "Content-Type": "application/json"
-    };
-    if(headers) {
-        queryHeaders = _.assignIn(queryHeaders, headers);
-    }
-    return fetch(url, {
-        method: 'GET',
-        mode: 'cors',
-        headers: queryHeaders
-    }).then(resp => {
-        setAuthHeaders(resp);
-        return resp.json();
-    });
+  let queryHeaders = {
+    "Content-Type": "application/json"
+  };
+  if (headers) {
+    queryHeaders = _.assignIn(queryHeaders, headers);
+  }
+  return fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    headers: queryHeaders
+  }).then(resp => {
+    setAuthHeaders(resp);
+    return resp.json();
+  });
 }
 
 function setAuthHeaders(resp) {
-    let headers = resp.headers;
-    if(headers.get('access-token') && headers.get('client')) {
-        localStorage.setItem('authHeaders', JSON.stringify({
-          "access-token": headers.get('access-token'),
-          "client": headers.get('client'),
-          "uid": headers.get('uid')
-        }));
-    }
+  let headers = resp.headers;
+  if (headers.get('access-token') && headers.get('client')) {
+    localStorage.setItem('authHeaders', JSON.stringify({
+      "access-token": headers.get('access-token'),
+      "client": headers.get('client'),
+      "uid": headers.get('uid')
+    }));
+  }
 }
