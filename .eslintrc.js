552e00ba953e61ac017458a3030affd448e2cbfe
Richard Xia <richardxia@richardxia.com>
2017-06-04 14:22:29 -0700

    Enforce linting top-level init.js file.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "env": {
+    "browser": true
+  },
+  "rules": {
+    "react/jsx-filename-extension": 0
+  }
 };
--- app/init.js
+++ app/init.js
@@ -1,36 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import { syncHistoryWithStore } from 'react-router-redux';
 
-import App from './components/App';
-import CategoryPage from './components/Find/FindPage';
-import ResourcesTable from './components/Search/ResourcesTable';
-import Resource from './components/Resource/Resource';
-import EditSections from './components/Edit/EditSections';
-import Login from './components/User/Login';
-import CreateAccount from './components/User/CreateAccount';
-import TestAuth from './components/User/TestAuth';
-
-
-import Google from './utils/google';
-
+import configureStore from './store/configureStore';
 import routes from './routes';
 
 require('./styles/main.scss');
 
 const store = configureStore();
-
-const history = syncHistoryWithStore(browserHistory, store)
-
-
-function redirectToRoot (nextState, replace) {
-  replace({
-    pathname: '/',
-  });
-};
+const history = syncHistoryWithStore(browserHistory, store);
 
 
 ReactDOM.render((
@@ -40,5 +20,3 @@ ReactDOM.render((
     </Router>
   </Provider>
 ), document.getElementById('root'));
-
-
