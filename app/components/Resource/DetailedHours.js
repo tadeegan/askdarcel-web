4ebf127d7ec62878b23571de9f6a60620d1ce35a
Richard Xia <richardxia@richardxia.com>
2017-06-04 15:54:53 -0700

    Use better array key.

--- app/components/Resource/DetailedHours.js
+++ app/components/Resource/DetailedHours.js
@@ -6,17 +6,16 @@ export default function DetailedHours(props) {
   let { schedule } = props;
   schedule = sortScheduleDays(schedule);
   const hoursList = schedule.map((item) => {
-    const key = `${item.day}-${item.opens_at}-${item.closes_at}`;
     if (item.opens_at === 0 && item.closes_at >= 2359) {
       return (
-        <div key={key} className="weekly-hours-list--item">
+        <div key={item.id} className="weekly-hours-list--item">
           <span className="weekly-hours-list--item--day">{`${item.day}`}</span>
           <span className="weekly-hours-list--item--hours">24 Hours</span>
         </div>
       );
     }
     return (
-      <div key={key} className="weekly-hours-list--item">
+      <div key={item.id} className="weekly-hours-list--item">
         <span className="weekly-hours-list--item--day">{`${item.day}`}</span>
         <span className="weekly-hours-list--item--hours">{`${timeToString(item.opens_at)} - ${timeToString(item.closes_at)}`}
         </span>
