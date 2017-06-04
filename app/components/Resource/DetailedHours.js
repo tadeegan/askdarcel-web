67762e7c91a8e9fe54b0078e10fdc0d05d7c5375
Richard Xia <richardxia@richardxia.com>
2017-06-04 15:09:35 -0700

    Fix up code style of Resource/DetailedHours.js

--- app/components/Resource/DetailedHours.js
+++ app/components/Resource/DetailedHours.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { timeToString, sortScheduleDays } from '../../utils/index';
 
-export default class DetailedHours extends Component {
-  render() {
-    let { schedule } = this.props;
-    schedule = sortScheduleDays(schedule);
-    let hoursList = schedule.map((item,i) =>
-      item.opens_at == 0 && item.closes_at >= 2359 ?
-      <div key={i} className="weekly-hours-list--item">
-      	<span className="weekly-hours-list--item--day">{`${item.day}`}</span>
-      	<span className="weekly-hours-list--item--hours">24 Hours</span>
-      </div> :
-      <div key={i} className="weekly-hours-list--item">
-     		<span className="weekly-hours-list--item--day">{`${item.day}`}</span>
-     		<span className="weekly-hours-list--item--hours">{`${timeToString(item.opens_at)} - ${timeToString(item.closes_at)}`}
-     		</span>
-     	</div>
-
-    );
+export default function DetailedHours() {
+  let { schedule } = this.props;
+  schedule = sortScheduleDays(schedule);
+  const hoursList = schedule.map((item) => {
+    const key = `${item.day}-${item.opens_at}-${item.closes_at}`;
+    if (item.opens_at === 0 && item.closes_at >= 2359) {
+      return (
+        <div key={key} className="weekly-hours-list--item">
+          <span className="weekly-hours-list--item--day">{`${item.day}`}</span>
+          <span className="weekly-hours-list--item--hours">24 Hours</span>
+        </div>
+      );
+    }
     return (
-      <span className="weekly-hours-list">
-        {hoursList}
-      </span>
+      <div key={key} className="weekly-hours-list--item">
+        <span className="weekly-hours-list--item--day">{`${item.day}`}</span>
+        <span className="weekly-hours-list--item--hours">{`${timeToString(item.opens_at)} - ${timeToString(item.closes_at)}`}
+        </span>
+      </div>
     );
-  }
+  });
+  return (
+    <span className="weekly-hours-list">
+      {hoursList}
+    </span>
+  );
 }
